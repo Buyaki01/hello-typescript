aff3a7d3e7ea2983dab088566aba5da710fbb8b1
Buyaki01 <rittahbuyaki@gmail.com>
2023-03-25 11:37:55 +0300

    Typescript Objects, Arrays, Tuples & Enums

--- build/js/src/main.js
+++ build/js/src/main.js
@@ -13,3 +13,63 @@ const sum = (a, b) => {
 let postID;
 let isActive;
 let re = /\w+/g;
+let stringArr = ['one', 'hey', 'Dave'];
+let guitars = ['strat', 'Les Paul', 5150];
+let mixedData = ['EVH', 1984, true];
+let test = [];
+let bands = [];
+bands.push("Van Halen");
+//Tuple
+let myTuple = ['Dave', 42, true];
+let mixed = ['John', 1, false];
+mixed = myTuple;
+myTuple[1] = 42;
+//Objects
+let myObj;
+myObj = [];
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: 'Dave',
+    prop2: true,
+};
+exampleObj.prop1 = 'John';
+let evh = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812']
+};
+let jp = {
+    name: 'Jimmy',
+    active: true,
+    albums: ['I', 'II', 'IV']
+};
+evh = jp;
+let eh = {
+    name: 'Piano',
+    albums: [1984, 5150, 'OU812']
+};
+const greetPionist = (pionist) => {
+    return `Hello ${pionist.name}!`;
+};
+console.log(greetPionist(eh));
+let violin = {
+    active: true,
+    albums: [1984, 5150, 'OU812']
+};
+const greetViolinist = (violinst) => {
+    if (violinst.name) {
+        return `Hello ${violinst.name.toUpperCase()}!`;
+    }
+    return "Hello!";
+};
+console.log(greetViolinist(violin));
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U); //starts from zero but in case you want it to start from 1, state U = 1
--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,103 @@ let postID: string | number
 let isActive: number | boolean | string
 
 let re: RegExp = /\w+/g
+
+let stringArr = ['one', 'hey', 'Dave']
+
+let guitars = ['strat', 'Les Paul', 5150]
+
+let mixedData = ['EVH', 1984, true]
+
+let test = []
+let bands: string[] = []
+
+bands.push("Van Halen")
+
+//Tuple
+let myTuple: [string, number, boolean] = ['Dave', 42, true]
+
+let mixed = ['John', 1, false]
+
+mixed = myTuple
+
+myTuple[1] = 42
+
+//Objects
+let myObj: object
+myObj =[]
+myObj = bands
+myObj = {}
+
+const exampleObj = {
+  prop1: 'Dave',
+  prop2: true,
+}
+
+exampleObj.prop1 = 'John'
+
+type Guitarist = {
+  name: string,
+  active: boolean,
+  albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+  name: 'Eddie', 
+  active: false,
+  albums: [1984, 5150, 'OU812']
+}
+
+let jp: Guitarist = {
+  name: 'Jimmy', 
+  active: true,
+  albums: ['I', 'II', 'IV']
+}
+
+evh = jp
+
+interface Pionist {
+  name: string,
+  active?: boolean,
+  albums: (string | number)[]
+}
+
+let eh: Pionist = {
+  name: 'Piano', 
+  albums: [1984, 5150, 'OU812']
+}
+
+const greetPionist = (pionist: Pionist) => {
+  return `Hello ${pionist.name}!`
+}
+
+console.log(greetPionist(eh))
+
+interface Violinist {
+  name?: string,
+  active: boolean,
+  albums: (string | number)[]
+}
+
+let violin: Violinist = {
+  active: true,
+  albums: [1984, 5150, 'OU812']
+}
+
+const greetViolinist = (violinst: Violinist) => {
+  if(violinst.name){
+    return `Hello ${violinst.name.toUpperCase()}!`
+  }
+  return "Hello!"
+}
+
+console.log(greetViolinist(violin))
+
+enum Grade{
+  U = 1,
+  D,
+  C,
+  B,
+  A,
+}
+
+console.log(Grade.U) //starts from zero but in case you want it to start from 1, state U = 1

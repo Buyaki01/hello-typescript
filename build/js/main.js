ff8b4accd40347bcd90a3c9d32b17a0cdeae4093
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-03 12:28:30 +0300

    Add Literal types

--- build/js/main.js
+++ build/js/main.js
@@ -2,3 +2,8 @@
 //Type Aliases
 //interface postId = stringOrNumber //It will result into an error, you cannot do this with interfaces. 
 // stringOrNumber only refers to a type, but is being used as a value here
+// Literal Types/ Specific Assignment
+let myName; //myName is of type Dave
+// myName = 'Ritta' //Will result to an error, type Ritta is not assignable to type Dave
+let userName;
+userName = 'Ritta';
--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,11 @@ type userId = stringOrNumber
 //interface postId = stringOrNumber //It will result into an error, you cannot do this with interfaces. 
 // stringOrNumber only refers to a type, but is being used as a value here
 
+// Literal Types/ Specific Assignment
+let myName: 'Dave' //myName is of type Dave
+
+// myName = 'Ritta' //Will result to an error, type Ritta is not assignable to type Dave
+
+let userName: 'Dave' | 'Ritta' | 'Sweta'
+
+userName = 'Ritta'

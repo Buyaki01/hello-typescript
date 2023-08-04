4dff76ca71eeb0c8db26f1d3fde1844e344e2277
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-04 17:09:30 +0300

    Add double casting

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 // Type assertions allow you to tell the TypeScript compiler that you know more about the type of a variable than it does
+// You are basically overruling TypeScript
 // You can use either angle brackets or the as keyword for type assertions
 //convert to more or less specific
 let a = 'hello';
@@ -15,4 +16,10 @@ const addOrConcat = (a, b, c) => {
         return a + b;
     return '' + a + b;
 };
+// let myVal: string = addOrConcat(2, 2, 'concat') //Will result in an error because myVal is a string and yet addOrConcat is a union of number | string
 let myVal = addOrConcat(2, 2, 'concat');
+// Be careful Ts sees no problem - but a string is returned
+let nextVal = addOrConcat(2, 2, 'concat');
+//Double casting and the unknown type
+// 10 as string // Will result into an error and TypeScript recommends, if you are intentional about what you are doing, convert the expression to unknown first
+10; //double assertions, double casting
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ type Two = string | number
 type Three = 'hello'
 
 // Type assertions allow you to tell the TypeScript compiler that you know more about the type of a variable than it does
+// You are basically overruling TypeScript
 // You can use either angle brackets or the as keyword for type assertions
 //convert to more or less specific
 let a: One = 'hello'
@@ -20,4 +21,14 @@ const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string
   return '' + a + b
 }
 
+// let myVal: string = addOrConcat(2, 2, 'concat') //Will result in an error because myVal is a string and yet addOrConcat is a union of number | string
 let myVal: string = addOrConcat(2, 2, 'concat') as string
+
+// Be careful Ts sees no problem - but a string is returned
+let nextVal: number = addOrConcat(2, 2, 'concat') as number
+
+//Double casting and the unknown type
+// 10 as string // Will result into an error and TypeScript recommends, if you are intentional about what you are doing, convert the expression to unknown first
+
+(10 as unknown) as string //double assertions, double casting
+

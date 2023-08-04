a86a66065f9fe2bdbeaa208367596ce8ed556d55
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-04 14:22:59 +0300

    Use angle brackets for type assertions

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,11 @@
 "use strict";
+// Type assertions allow you to tell the TypeScript compiler that you know more about the type of a variable than it does
+// You can use either angle brackets or the as keyword for type assertions
 //convert to more or less specific
 let a = 'hello';
 let b = a; // less specific
 console.log(b);
 let c = a; // more specific
+let d = 'World';
+console.log(d);
+let e = 'World'; // when working with React, using type assertions like this is not a common practice
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,16 @@ type One = string
 type Two = string | number
 type Three = 'hello'
 
+// Type assertions allow you to tell the TypeScript compiler that you know more about the type of a variable than it does
+// You can use either angle brackets or the as keyword for type assertions
 //convert to more or less specific
 let a: One = 'hello'
 let b = a as Two // less specific
 console.log(b)
 let c = a as Three // more specific
+
+let d = <One>'World'
+console.log(d)
+let e = <string | number>'World' // when working with React, using type assertions like this is not a common practice
+
+

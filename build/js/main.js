acfdf353cb34e6b81a1ba40a6b5d53410fcb82b4
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-05 12:31:46 +0300

    Add getAge method to access private property age

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,8 @@
 "use strict";
 class Coder {
-    constructor(name, music, age, lang = 'Typescript') {
+    constructor(name, music, age, //Private: can only be accessed inside of this class
+    lang = 'Typescript' //protected: can be accessed inside of the class but it could also be accessed inside of derived classes
+    ) {
         this.name = name;
         this.music = music;
         this.age = age;
@@ -10,5 +12,11 @@ class Coder {
         this.age = age;
         this.lang = lang;
     }
+    getAge() {
+        return `Hello, I'm ${this.age}`;
+    }
 }
 const Dave = new Coder('Dave', 'Rock', 42);
+console.log(Dave.getAge()); //while we can access a method that accesses the age value inside of the class, we can't access those properties directly outside of the class
+console.log(Dave.age); //age is private and only accessible within class 'Coder'
+console.log(Dave.lang); //lang is protected and only accessible within class 'Coder' and its subclasses
--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,21 @@ class Coder {
   constructor(
     public readonly name: string, 
     public music: string, 
-    private age: number, 
-    protected lang: string = 'Typescript'
+    private age: number, //Private: can only be accessed inside of this class
+    protected lang: string = 'Typescript' //protected: can be accessed inside of the class but it could also be accessed inside of derived classes
   ) {
     this.name = name
     this.music = music
     this.age = age
     this.lang = lang
   }
+
+  public getAge() {
+    return `Hello, I'm ${this.age}`
+  }
 }
 
 const Dave = new Coder('Dave', 'Rock', 42)
+console.log(Dave.getAge()) //while we can access a method that accesses the age value inside of the class, we can't access those properties directly outside of the class
+console.log(Dave.age) //age is private and only accessible within class 'Coder'
+console.log(Dave.lang) //lang is protected and only accessible within class 'Coder' and its subclasses

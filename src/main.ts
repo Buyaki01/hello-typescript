4cb0084175c64beae0b646db19b17007b330f8c6
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-05 12:52:09 +0300

    Extends for subclasses

--- src/main.ts
+++ src/main.ts
@@ -21,5 +21,27 @@ class Coder {
 
 const Dave = new Coder('Dave', 'Rock', 42)
 console.log(Dave.getAge()) //while we can access a method that accesses the age value inside of the class, we can't access those properties directly outside of the class
-console.log(Dave.age) //age is private and only accessible within class 'Coder'
-console.log(Dave.lang) //lang is protected and only accessible within class 'Coder' and its subclasses
+// console.log(Dave.age) //age is private and only accessible within class 'Coder'
+// console.log(Dave.lang) //lang is protected and only accessible within class 'Coder' and its subclasses
+
+class WebDev extends Coder {
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age)
+    this.computer = computer
+  }
+
+  public getLang() {
+    return `I write ${this.lang}`
+  }
+}
+
+const Ritta = new WebDev('Mac', 'Ritta', 'soft music', 27)
+console.log(Ritta.getLang())
+// console.log(Ritta.age)
+// console.log(Ritta.lang)
+

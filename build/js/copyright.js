0784ea3a514cfaaa79ff378c5b27821daa44e465
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-05 10:07:04 +0300

    Add solution to the student challenge

--- build/js/copyright.js
+++ build/js/copyright.js
@@ -1,6 +1,10 @@
 "use strict";
 // Original JS code
+// const year = document.getElementById("year")
+// const thisYear = new Date().getFullYear()
+// year.setAttribute("datetime", thisYear)
+// year.textContent = thisYear
 const year = document.getElementById("year");
 const thisYear = new Date().getFullYear();
-year.setAttribute("datetime", thisYear);
-year.textContent = thisYear;
+year.setAttribute("datetime", thisYear.toString());
+year.textContent = thisYear.toString();
--- src/copyright.ts
+++ src/copyright.ts
@@ -1,5 +1,10 @@
 // Original JS code
-const year = document.getElementById("year")
+// const year = document.getElementById("year")
+// const thisYear = new Date().getFullYear()
+// year.setAttribute("datetime", thisYear)
+// year.textContent = thisYear
+
+const year = document.getElementById("year") as HTMLInputElement
 const thisYear = new Date().getFullYear()
-year.setAttribute("datetime", thisYear)
-year.textContent = thisYear
+year.setAttribute("datetime", thisYear.toString())
+year.textContent = thisYear.toString()

0572399bf688df25697e96017bd6fe7a0e173136
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-07 10:57:26 +0300

    Add instances that would result in an error as a result of not having index signatures

--- src/main.ts
+++ src/main.ts
@@ -1,121 +1,29 @@
-class Coder {
+// Index Signatures
+// They are useful when you are creating an object but you don't know the exact names of the object keys but you do know the shape of the object and you can declare the type of the keys and the types of the values
+// TypeScript requires an index signature if you attempt to access an object property dynamically 
 
-  secondLang!: string
-
-  constructor(
-    public readonly name: string, 
-    public music: string, 
-    private age: number, //Private: can only be accessed inside of this class
-    protected lang: string = 'Typescript' //protected: can be accessed inside of the class but it could also be accessed inside of derived classes
-  ) {
-    this.name = name
-    this.music = music
-    this.age = age
-    this.lang = lang
-  }
-
-  public getAge() {
-    return `Hello, I'm ${this.age}`
-  }
+interface TransactionObj {
+  Pizza: number,
+  Books: number,
+  Job: number
 }
 
-const Dave = new Coder('Dave', 'Rock', 42)
-console.log(Dave.getAge()) //while we can access a method that accesses the age value inside of the class, we can't access those properties directly outside of the class
-// console.log(Dave.age) //age is private and only accessible within class 'Coder'
-// console.log(Dave.lang) //lang is protected and only accessible within class 'Coder' and its subclasses
-
-class WebDev extends Coder {
-  constructor(
-    public computer: string,
-    name: string,
-    music: string,
-    age: number
-  ) {
-    super(name, music, age)
-    this.computer = computer
-  }
-
-  public getLang() {
-    return `I write ${this.lang}`
-  }
+const todaysTransactions: TransactionObj = {
+  Pizza: -10,
+  Books: -5,
+  Job: 50
 }
 
-const Ritta = new WebDev('Mac', 'Ritta', 'soft music', 27)
-console.log(Ritta.getLang())
-// console.log(Ritta.age)
-// console.log(Ritta.lang)
+console.log(todaysTransactions.Pizza)
+console.log(todaysTransactions['Pizza'])
 
-interface Musician {
-  name: string,
-  instrument: string,
-  play(action: string): string
+let prop: string = 'Pizza'
+console.log(todaysTransactions[prop])
+
+const todaysNet = (transactions: TransactionObj): number => {
+  let total = 0
+  for (const transaction in transactions) {
+    total += transactions[transaction]
+  }
+  return total
 }
-
-class Guitarist implements Musician {
-  name: string
-  instrument: string
-
-  constructor(name: string, instrument: string) {
-    this.name = name
-    this.instrument = instrument
-  }
-
-  play(action: string) {
-    return `${this.name} ${action} the ${this.instrument}`
-  }
-}
-
-const Page = new Guitarist('Jimmy', 'guitar')
-console.log(Page.play('strums'))
-
-class Peeps {
-  // static applies directly to the class and not to any specific object that you instantiate with the class
-  static count: number = 0 //static means count does not apply to any instantiation of the class it applies to the class directly itself.
-  // we'll be keeping track of count in the class not in any one instance of the class 
-  static getCount(): number {
-    return Peeps.count // notice here we don't use the keyword this, we refer to the class itself, Peeps.count
-  }
-
-  public id:number
-
-  constructor(public name:string) {
-    this.name = name
-    this.id = ++Peeps.count
-  }
-}
-
-const John = new Peeps('John')
-const Steve = new Peeps('Steve')
-const Amy = new Peeps('Amy')
-
-console.log(Amy.id)
-console.log(Steve.id)
-console.log(John.id)
-console.log(Peeps.count)
-
-// Getters and Setters
-class Bands {
-  private dataState: string[] //This will be our state that we are keeping track of in bands and it's going to be an array with string data
-
-  constructor() {
-    this.dataState = []
-  }
-
-  public get data(): string[] {
-    return this.dataState
-  }
-
-  public set data(value: string[]) {
-    if (Array.isArray(value) && value.every(el => typeof el === 'string')){
-      this.dataState = value
-      return 
-    } else throw new Error('Param is not an array of strings')
-  }
-}
-
-const MyBands = new Bands()
-MyBands.data = ['Agape Gospel', 'Adawange']
-console.log(MyBands.data)
-MyBands.data = [...MyBands.data, 'Timiza']
-console.log(MyBands.data)
-MyBands.data = ['Uwezo']

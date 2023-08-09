1c652c85a96f05e3b3fd8dea41ef0de090e1b75e
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-09 08:08:15 +0300

    Add isObj example

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,11 @@
 "use strict";
 // const stringEcho = (arg: string): string => arg //This only accepts data type of string
 const echo = (arg) => arg; // works with any type we would pass into this function
+const isObj = (arg) => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null); //null and array also returns type of object
+};
+console.log(isObj(true));
+console.log(isObj('John'));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: 'John' }));
+console.log(isObj(null));
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
 // const stringEcho = (arg: string): string => arg //This only accepts data type of string
 const echo = <T>(arg: T): T => arg // works with any type we would pass into this function
+
+const isObj = <T>(arg: T): boolean => {
+  return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null) //null and array also returns type of object
+}
+
+console.log(isObj(true))
+console.log(isObj('John'))
+console.log(isObj([1,2,3]))
+console.log(isObj({ name: 'John' }))
+console.log(isObj(null))
